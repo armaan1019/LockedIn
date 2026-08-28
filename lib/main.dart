4213164769b98c26df26dd15920b40d0b578e7bb
Armaan Sharma <armaan192004@gmail.com>
2026-08-27 20:26:14 -0700

    App check hopefully working

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'core/providers/app_provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseAppCheck.instance.activate(
+    providerApple: kDebugMode
+        ? const AppleDebugProvider()
+        : const AppleAppAttestProvider(),
+  );
 
   runApp(MultiProvider(providers: appProviders, child: const MyApp()));
 }
